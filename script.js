5182a369b4e7531523b4524eef5923b3d472ad77
unknown <msolano@intellisys.com.do>
2018-07-05 17:08:58 -0400

    Creating a general index variable and eleminating the variable idx on modal.slide

--- script.js
+++ script.js
@@ -3,37 +3,33 @@ var modal = {
   currentSlide: null,
   autoPlay: false,
   SLIDE_TIMEOUT: 2000,
+  indx: null,
   slides: [
     {
-      idx: 0,
       title: 'A stormy sky and ocean',
       imgSrc: "img1.jpg",
       description: 'Fort Ebey State Park is a 649-acre camping park on Whidbey Island.',
       reference: 'Fort Ebey State Park, Coupeville, United States by Patrick Fore.'
     },
     {
-      idx: 1,
       title: 'Dachstein Mountains',
       imgSrc: "img2.jpg",
       description: 'The Dachstein Mountains are a mountain range in the Northern Limestone Alps. The term is used by the Austrian Alpine Club in its classification of the Eastern Alps as one of the 24 sub-ranges of the Northern Limestone Alps.',
       reference: 'Dachstein Mountains, Obertraun, Austria by Kaidi Guo'
     },
     {
-      idx: 2,
       title: '',
       imgSrc: "img3.jpg",
       description: '',
       reference: 'Tõrva, Estonia by Jaak Horn'
     },
     {
-      idx: 3,
       title: 'Lutry, Switzerland',
       imgSrc: "img4.jpg",
       description: 'Lutry is a municipality in the Swiss canton of Vaud, located in the Lavaux-Oron.',
       reference: 'Lutry, Switzerland by Olivier Fahrni'
     },
     {
-      idx: 4,
       title: 'Mirror Lake Trail - Tosemite National Park',
       imgSrc: "img5.jpg",
       description: 'From the shuttle stop (#17), a paved trail leads directly to Mirror Lake. Which is in the Adirondack Mountains in northern New York in the United States. The lake is approximately 124 acres, with a watershed area of 741 acres.',
@@ -47,6 +43,7 @@ var modal = {
     init: function () {   
       if(modal.slides != null){
         modal.currentSlide = modal.slides[0];
+        modal.indx = 0;
 
         viewSlide.init();
         viewThumbnail.init();
@@ -67,23 +64,25 @@ var modal = {
     },
 
     getNextSlide: function () {
-      var nextIdx = modal.currentSlide.idx;
+      var nextIdx = modal.indx;
       nextIdx++;
 
       if(nextIdx >= modal.slides.length){
         nextIdx = 0;
       }
-
+      modal.indx = nextIdx;
       return modal.slides[nextIdx];
     },
 
     getPrevSlide: function () {
-      var prevIdx = modal.currentSlide.idx;
+      var prevIdx = modal.indx;
       prevIdx--;
+      
       if(prevIdx < 0){
         prevIdx = (modal.slides.length-1);
       }
 
+      modal.indx = prevIdx;
       return modal.slides[prevIdx];
     },
 
@@ -195,7 +194,7 @@ var modal = {
     },
 
     render: function () {
-      var counter = modal.currentSlide.idx + 1;
+      var counter = modal.indx + 1;
       this.Counter.innerHTML = counter + "/" + modal.slides.length;
     },
 
